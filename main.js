c2d18f21e422dc253c4a40480fd17fd5a7c09981
Hamser1001 <upscale.mind50@gmail.com>
2024-07-23 01:40:46 +0100

    some codes etc

---
+++ main.js
@@ -0,0 +1,6 @@
+let element = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+console.log(document.documentElement.scrollHeight);
+console.log(document.documentElement.clientHeight);
+console.log();
