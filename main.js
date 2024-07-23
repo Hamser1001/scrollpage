010fe7190091a38dceb58de90824977d5b8abc0f
Hamser1001 <upscale.mind50@gmail.com>
2024-07-23 01:48:00 +0100

    finishing code

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ let height = document.documentElement.scrollHeight - document.documentElement.cl
 
 console.log(document.documentElement.scrollHeight);
 console.log(document.documentElement.clientHeight);
-console.log();
+console.log(height);
+
+window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    element.style.width = `${(scrollTop / height) * 100}%`;
+});
